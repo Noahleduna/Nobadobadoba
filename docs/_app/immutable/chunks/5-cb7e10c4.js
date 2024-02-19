a69a44ae75e8d67d069bb4e45a158a788f5fe830
Noah De Luna <noadel@elevad.umea.se>
2024-02-19 12:04:21 +0100

    Klar med start

---
+++ docs/_app/immutable/chunks/5-cb7e10c4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-33f719a7.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,29 @@
+<script>
+let melvin=3
+</script>
 
 <h1>
-  Välkommen till min hemsida!
+  Välkommen till min hemsida! Milthons hemsida är sämre!
 </h1>
 
 <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
 
-<h3 >Förnamn Efternamn</h3>
+<h3 >Noah de Luna</h3>
 
+<p style = 'color:aquamarine;font-size:2em'> Hej jag gillar mat</p>
+
+<tex class='rubriker'>Hej hallojsan melvin och milthon äter pankakor</tex>
+<style>
+h1{
+  color:purple
+}
+h2{
+  color:#4F3BA0
+}
+.rubriker{
+  color:rgba(0, 0, 0, 0.465);
+  font-size: 2em;
+  font-family: 'Gill Sans', cursive;
+}
+
+</style>
